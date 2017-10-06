679d84689e2d88f4d4285edb18036f2436f440d2
Richard von Seck <richard.von-seck@stusta.net>
2017-10-06 16:55:16 +0200

    plugins/jemand.py: Added random person selector plugin

---
+++ plugins/jemand.py
@@ -0,0 +1,19 @@
+import random
+from matrix_bot_api.mregex_handler import MRegexHandler
+
+HELP_DESC = ("(automatic)\t\tThe bot replaces 'jemand™' in a sentence with a "
+             "random room member")
+
+def register_to(bot):
+
+    # Echo back the given command
+    def jemand_callback(room, event):
+        members = room.get_joined_members()
+        jemand = random.choice(list(members.keys()))
+        msg = event['content']['body']
+        msg = msg.replace("jemand™", jemand)
+        room.send_text(msg)
+
+    # Add a command handler waiting for the echo command
+    jemand_handler = MRegexHandler("jemand™", jemand_callback)
+    bot.add_handler(jemand_handler)
