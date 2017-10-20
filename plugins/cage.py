6d66f81f0c9d87e317478acbf1bb142c5fe8283a
Janosch Kindl <jk@stusta.de>
2017-10-20 20:27:36 +0200

    Updated maunz and did added cage

--- plugins/cage.py
+++ plugins/cage.py
@@ -3,18 +3,18 @@ from matrix_bot_api.mcommand_handler import MCommandHandler
 import requests
 
 
-HELP_DESC = (""""!maunz                 -   miau""")
+HELP_DESC = (""""!cage                 -   Nicolas Cage""")
 
 upload_url = 'https://matrix.org/_matrix/media/r0/upload'
 
-download_url = 'https://loremflickr.com/'
+download_url = 'https://www.placecage.com/'
 
 def register_to(bot):    
-    def maunz_callback(room, event):
+    def cage_callback(room, event):
         args = event['content']['body'].split()
         # remove the command
         args.pop(0)
-        
+
         if(len(args) < 2):
             r = requests.get(download_url + '300/200')
         else:
@@ -27,9 +27,9 @@ def register_to(bot):
             up = room.client.api.media_upload(img, typ)
             print('Uploaded', up)
             uri = up['content_uri']
-            room.send_image(uri, 'Miau')
+            room.send_image(uri, 'Hübschlon')
         else:
-            room.send_text('Kittens ' + download_url + ' unavailable :( ')
+            room.send_text('Cage ' + download_url + ' unavailable :( ')
 
-    maunz_handler = MCommandHandler("maunz", maunz_callback)
-    bot.add_handler(maunz_handler)
+    cage_handler = MCommandHandler("cage", cage_callback)
+    bot.add_handler(cage_handler)
