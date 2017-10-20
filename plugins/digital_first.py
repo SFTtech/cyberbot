109b6def17dae82b43d6bed4f9fd032c7f8bbc4f
Richard von Seck <richard.von-seck@stusta.net>
2017-10-20 18:52:23 +0200

    plugins/{industry,digital}.py: Counters now saved in sqlite dtb

--- plugins/digital_first.py
+++ plugins/digital_first.py
@@ -1,3 +1,4 @@
+import sqlite3
 from matrix_bot_api.mcommand_handler import MCommandHandler
 
 HELP_DESC = ("!digital++\t\t\t\t\t-\tIncrease the digitalization counter"
@@ -23,21 +24,46 @@ def register_to(bot):
             room.send_text("This feature is not available in this room")
             return
 
-        # save and update the counterstate in a 'digital_counter' file
-        try:
-            with open('digital_counter', 'r+') as f:
-                n = int(f.read(1024))
-                f.seek(0)
-                n += 1
-                f.write(str(n))
-        except FileNotFoundError:
-            with open('digital_counter', 'x') as f:
-                n = 0
-                f.write(str(n))
+        # save and update the counterstate in the counter database
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute("update {}".format(COUNTER_TAB)
+                  + " set counter=counter+1 where name = 'digital'")
+        conn.commit()
+        c.execute("select counter from {}".format(COUNTER_TAB)
+                  + " where name = 'digital'")
+        n = c.fetchall()[0][0]
+        conn.close()
 
         first = get_rank_string(n)
         second = get_rank_string(n+1)
         room.send_text("Digital {}, Bedenken {}.".format(first, second))
+
+
+    # initialize sqlite counter entry for this plugin, if not already existing
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+
+    try:
+        c.execute("select name from {}".format(COUNTER_TAB)
+                  + " where name = 'digital'")
+
+        if (c.fetchall() == []):
+            c.execute("insert into {}".format(COUNTER_TAB)
+                      + " values ('digital', 0)")
+
+    except sqlite3.OperationalError as e:
+        if (e.args[0].find('no such table') != -1):
+            c.execute("create table {}".format(COUNTER_TAB)
+                      + " (name text, counter integer)")
+
+            c.execute("insert into {}".format(COUNTER_TAB)
+                      + " values ('digital', 0)")
+        else:
+            print("Encountered miscellaneous sqlite error:", e)
+
+    conn.commit()
+    conn.close()
 
     digital_handler = MCommandHandler("digital\+\+", digital_callback)
     bot.add_handler(digital_handler)
--- plugins/industrie.py
+++ plugins/industrie.py
@@ -1,3 +1,4 @@
+import sqlite3
 from matrix_bot_api.mcommand_handler import MCommandHandler
 
 HELP_DESC = "!industrie++\t\t\t\t\t-\tHeralds the dawn of a new industrial era"
@@ -11,19 +12,44 @@ def register_to(bot):
             room.send_text("This feature is not available in this room")
             return
 
-        # save and update the counterstate in a 'digital_counter' file
-        try:
-            with open('industry_age', 'r+') as f:
-                n = int(f.read(1024))
-                f.seek(0)
-                n += 1
-                f.write(str(n))
-        except FileNotFoundError:
-            with open('industry_age', 'x') as f:
-                n = 0
-                f.write(str(n))
+        # save and update the counterstate in the sqlite database
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute("update {}".format(COUNTER_TAB)
+                  + " set counter=counter+1 where name = 'industrie'")
+        conn.commit()
+        c.execute("select counter from {}".format(COUNTER_TAB)
+                  + " where name = 'industrie'")
+        n = c.fetchall()[0][0]
+        conn.close()
 
         room.send_text("Willkommen im Zeitalter von Industrie {}.0!".format(n))
+
+
+    # initialize sqlite counter entry for this plugin, if not already existing
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+
+    try:
+        c.execute("select name from {}".format(COUNTER_TAB)
+                  + " where name = 'industrie'")
+
+        if (c.fetchall() == []):
+            c.execute("insert into {}".format(COUNTER_TAB)
+                      + " values ('industrie', 0)")
+
+    except sqlite3.OperationalError as e:
+        if (e.args[0].find('no such table') != -1):
+            c.execute("create table {}".format(COUNTER_TAB)
+                      + " (name text, counter integer)")
+
+            c.execute("insert into {}".format(COUNTER_TAB)
+                      + " values ('industrie', 0)")
+        else:
+            print("Encountered miscellaneous sqlite error:", e)
+
+    conn.commit()
+    conn.close()
 
     industry_handler = MCommandHandler("industrie\+\+", industry_callback)
     bot.add_handler(industry_handler)
