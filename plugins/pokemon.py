688afe6efa6b0e4ac70d8c9097521baf3964788e
Janosch Kindl <jk@stusta.de>
2017-10-22 18:39:56 +0200

    Improved pokemon output

--- plugins/pokemon.py
+++ plugins/pokemon.py
@@ -1,28 +1,33 @@
 from matrix_bot_api.mcommand_handler import MCommandHandler
 
 import requests
+from random import choice
 
 
-HELP_DESC = ("!pokemon\t\t\t\t\t-\tRetrieves the name of a pokemon by number. Eg. !pokemon 4 yields Glumanda.\n")
+HELP_DESC = ("!pokemon\t\t\t\t\t-\tRetrieves the name of a Pokémon by number\n")
 
 download_url = 'http://pokeapi.co/api/v2/pokemon/'
+
+# Feel free to add
+insults = ['mac using faggot', 'millenial', 'blazingly fast web developer', 'Apple Macintosh employing non-cisgender male']
 
 def register_to(bot):    
     def pokemon_callback(room, event):
         args = event['content']['body'].split()
-        
+
         if(len(args) < 2):
             r = requests.get(download_url + '1')
         else:
             r = requests.get(download_url + args[1])
 
         if(r):
+            insult = choice(insults)
             name = r.json()['name'].title()
             num = int(args[1])
             if(num > 151):
-                room.send_text('There are no pokemon above 150, but if there were, its name would be ' + name + ' you mac using faggot.')
+                room.send_text('There are no Pokémon above 150, but if there were, the name of № ' + args[1] + ' would be ' + name + ' you ' + insult)
             else:
-                room.send_text('Pokemon Nr. ' + args[1] + ' ' + name)
+                room.send_text('Pokémon № ' + args[1] + ' is ' + name)
 
     pokemon_handler = MCommandHandler("pokemon", pokemon_callback)
     bot.add_handler(pokemon_handler)
