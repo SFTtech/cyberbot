4449d9cc22975edfd4d3212f27ba8b64ee3f76eb
Richard von Seck <richard.von-seck@gmx.net>
2017-12-24 18:34:29 +0100

    plugins/danke.py: Added response module for gratitude avowals

---
+++ plugins/danke.py
@@ -0,0 +1,26 @@
+from random import choice
+from matrix_bot_api.mregex_handler import MRegexHandler
+
+HELP_DESC = "(automatic)\t\tThe bot responds on avowals of gratitude."
+
+avowals = ["You're welcome!",
+           "Don't mention it!",
+           "My pleasure!"]
+
+def register_to(bot):
+
+    # Echo back the given command
+    def danke_callback(room, event):
+        args = event['content']['body'].split()
+        args.pop(0)
+
+        t = choice(avowals)
+
+        room.send_text(event['sender'] + ': ' + t)
+
+    # Add a command handler waiting for the echo command
+    danke_handler = MRegexHandler("^(.|\s)*"
+                                  +"((([vV]ielen)?[dD]ank(e?))"
+                                  +"|([tT](hanks|hx)))(.|\s)*"
+                                  +"([bB]ernd)(.|\s)*$", danke_callback)
+    bot.add_handler(danke_handler)
