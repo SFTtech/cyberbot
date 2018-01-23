0ee8b1f215b0fc6f7f5f04c3d2766ac80a86d3b2
Richard von Seck <richard.von-seck@gmx.net>
2018-01-23 16:48:03 +0100

    plugins/jemand.py: Fixed plugin using new API

--- plugins/jemand.py
+++ plugins/jemand.py
@@ -9,9 +9,9 @@ def register_to(bot):
     # Echo back the given command
     def jemand_callback(room, event):
         members = room.get_joined_members()
-        jemand = random.choice(list(members.keys()))
+        jemand = random.choice(list(members))
         msg = event['content']['body']
-        msg = msg.replace("jemand™", jemand)
+        msg = msg.replace("jemand™", jemand.user_id)
         room.send_text(msg)
 
     # Add a command handler waiting for the echo command
