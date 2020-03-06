1d76215a24acca808600bf87cf7967adaa06d753
root <root@vmrbg348.in.tum.de>
2020-03-06 16:17:56 +0100

    add sendtext and get_joined_members to matrixroom

--- bernd/matrixroom.py
+++ bernd/matrixroom.py
@@ -1,15 +1,16 @@
 import asyncio
 import nio
 
+nio.RoomMember.get_friendly_name = lambda self: self.display_name
 
-class MatrixRoom:
+class MatrixRoom():
 
     def __init__(self, client, nio_room):
         self.client = client
         self.nio_room = nio_room
 
 
-    async def send_html(self, formatted_txt, txt):
+    async def send_html(self, formatted_txt, txt=""):
         #print(formatted_txt)
         #print(txt)
         response = await self.client.room_send(
@@ -23,3 +24,18 @@ class MatrixRoom:
                 },
                 ignore_unverified_devices=True)
         print(response)
+
+    async def send_text(self, txt):
+        response = await self.client.room_send(
+                room_id=self.nio_room.room_id,
+                message_type="m.room.message",
+                content={
+                    "msgtype": "m.text",
+                    "body": txt,
+                },
+                ignore_unverified_devices=True)
+        print(response)
+
+    async def get_joined_members(self):
+       k = await self.client.joined_members(self.nio_room.room_id)
+       return k.members
